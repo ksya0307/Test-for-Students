339bfa9555b53056fa3f7a562b3c46ccd0b65e57
Ксения Алексеева <s239828@std.novsu.ru>
2021-04-09 23:09:21 +0300

    add a bit to q'S

--- src/pr29_30/Question.java
+++ src/pr29_30/Question.java
@@ -63,5 +63,54 @@ public class Question {
         return null;
     }
 
+    public Map<Integer,String> getAnswers(int idQuestion) throws ClassNotFoundException {
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        String hostname = "localhost";
+        String user = "ksyaVova";
+        String pass = "sys";
+        String sid = "orcl";
+        Map<Integer,String> answers = new HashMap<Integer,String>();
+        Connection con = null;
+        Statement st = null;
+        String url = "jdbc:oracle:thin:@" + hostname + ":1521:" + sid;
+
+        try {
+            String sql = "select id,text from answers where idquestion=" + idQuestion;
+            System.out.println("Подключаемся к БД");
+            con = DriverManager.getConnection(url, user, pass);
+            System.out.println("Успешно");
+            st = con.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            System.out.print("\n");
+            if (rs != null) {
+
+                while (rs.next()) {
+
+                    answers.put(rs.getInt("id"),rs.getString("text"));
+
+                }
+                return answers;
+
+
+            } else System.out.println("Ошибочка!");
+
+
+        } catch (SQLException var33) {
+            System.out.println(var33.toString());
+        } finally {
+            if (con != null) {
+                System.out.println("Закрытие подключения");
+
+                try {
+                    con.close();
+                    return answers;
+                } catch (SQLException e) {
+                    System.out.println(e.toString());
+                }
+            }
+        }
+        return null;
+    }
+
 }
 
