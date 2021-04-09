8a9dc202169133291d56b550b6e677ea63e40097
Ксения Алексеева <s239828@std.novsu.ru>
2021-04-09 22:24:19 +0300

    add old

--- src/pr29_30/thetest.java
+++ src/pr29_30/thetest.java
@@ -46,7 +46,7 @@ public class thetest extends JFrame{
 
        // contentPane.add(scrollPane);
 //git
-
+//
         int y=10;
         for (Map.Entry<Integer, String> entry : questions.entrySet()
              ) {
